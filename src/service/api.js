c57807fd146c19f128f6649589a9cb666c203baa
jingang.wang <jingang.wang@fir.ai>
2019-10-16 20:19:39 +0800

    axios .env...

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import service from './index';
 
 export const login = (payload) => {
-  return service.post('/api/login/',{ data: payload });
+  return service.post('/api/login/', { data: payload });
 };
 export const logout = (payload) => {
   return service.get('/api/logout/', { params: payload });
--- src/service/index.js
+++ src/service/index.js
@@ -1,29 +1,40 @@
 import axios from 'axios';
 import { getToken } from '../utils/auth';
+import qs from 'qs'
+
 
 // 创建axios实例
 const service = axios.create({
   // baseURL: 'http://localhost:3000',
   baseURL: 'http://localhost:4001',
-  timeout: 15000 // 请求超时时间
+  timeout: 15000, // 请求超时时间,
+  transformRequest: [function (data) {
+    return qs.stringify(data)
+  }],
 });
 
 // request.interceptors
-service.interceptors.request.use(config => {
-  config.headers['loginToken'] = getToken(); // 让每个请求携带自定义token 请根据实际情况自行修改
-  return config;
-}, error => {
+service.interceptors.request.use(
+  (config) => {
+    config.headers['loginToken'] = getToken(); // 让每个请求携带自定义token 请根据实际情况自行修改
+
+    return config;
+  },
+  (error) => {
+
   // Do something with request error
   console.log('request: error==>>', error);// for debug
+
   return Promise.reject(error);
-});
+  }
+);
 
 // response.interceptors
 service.interceptors.response.use(
-  response => {
+  (response) => {
     return response.data;
   },
-  error => {
+  (error) => {
 
     if(error.response){
       console.log('error.response', error.response);
