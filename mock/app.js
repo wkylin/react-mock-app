accc58550053efb0a0332c7c01398ea1c04f9c89
jingang.wang <jingang.wang@fir.ai>
2019-10-16 20:00:45 +0800

    response request...

--- mock/app.js
+++ mock/app.js
@@ -7,7 +7,8 @@ const index = require("./routes/index");
 
 const app = express();
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // axios withCredentials: true时不能使用*
+  // res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, DNT, User-Agent, Keep-Alive, loginToken');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
