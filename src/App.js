23beeed66909ce8e50a8515df5ccf60777deff5d
wkylin.w <wkylin.w@gmail.com>
2019-10-19 11:00:46 +0800

    proxy ...master

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
 import './App.css';
 // import axios from 'axios'
-import { login, serve504, serve500 } from './service/api'
+import { login, serve504, serve500 , logout, corsLoginCheck, corsLogin} from './service/api'
 function App() {
 
   useEffect(() => {
     // axios.get(`http://localhost:4001/`)
     // axios.get(`http://localhost:4001/api/notfound`)
     // axios.get(`http://localhost:4001/api/login/`)
-    login({userName:'wkylin'})
+    // login({userName:'wkylin'})
+    logout({userName:'wkylin'})
+    // corsLoginCheck({userName:'wkylin'})
+    // corsLogin({type: 'account', username_no: '15026835870', passwd: 'Test123456'})
     // serve504()
       .then(res => {
         console.log('res==>>', res);
--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import service from './index';
 
 export const login = (payload) => {
-  return service.post('/api/login/', { data: payload });
+  return service.post('/api/login/', payload );
 };
 export const logout = (payload) => {
   return service.get('/api/logout/', { params: payload });
@@ -11,4 +11,10 @@ export const serve504 = (payload) => {
 };
 export const serve500 = (payload) => {
   return service.get('/api/serve500', { params: payload });
+};
+export const corsLoginCheck = (payload) => {
+  return service.get('/account/user/login/check/', { params: payload });
+};
+export const corsLogin = (payload) => {
+  return service.post('/account/user/signin/', payload );
 };
