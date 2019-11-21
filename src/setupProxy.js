a2b99d93537405eb7435cf7503354fbb9bb10d5e
jingang.wang <jingang.wang@fir.ai>
2019-11-21 17:51:35 +0800

    redux redux-saga ...master

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,22 @@
 const proxy = require('http-proxy-middleware');
-module.exports = function (app) {
+module.exports = function(app) {
   // app.use('/api', proxy({ target: 'http://localhost:40001', changeOrigin: true, })); // 官网的配置是错的
-  app.use(proxy('/api',{ target: 'http://localhost:4001', ws: false, changeOrigin: true, }));
-  app.use(proxy('/user',{ target: 'http://localhost:4005', ws: false, changeOrigin: true, }));
+  app.use(
+    proxy('/api', {
+      target: 'http://localhost:4001',
+      ws: false,
+      changeOrigin: true
+    })
+  );
+  app.use(
+    proxy('/user', {
+      target: 'http://localhost:4005',
+      ws: false,
+      changeOrigin: true
+    })
+  );
 };
 
 // package.json 访问接口
 // "proxy": "http://localhost:4001",
-// "proxy":"https://cyprexsvc.fir.ai",
+// "proxy":"https://dev.ai",
